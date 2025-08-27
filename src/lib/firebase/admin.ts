c1b6b5fc58b6de2935a354f1ab1bef7e4f689fed
pachkapelmeney <nikitaplanes@gmail.com>
2025-08-27 11:11:56 +0300

    - env file fix

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -3,17 +3,30 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 if (!admin.apps.length) {
-  const serviceAccountPath = path.resolve(process.cwd(), '../SECRETS/conference-companion-d4f5a-firebase-adminsdk-fbsvc-c5f5e7dbb1.json');
-  
-  if (!fs.existsSync(serviceAccountPath)) {
-    throw new Error(`Service account key not found at ${serviceAccountPath}. Please ensure the SECRETS directory is at the root of the project.`);
+  // For firebase
+  if (process.env.FIREBASE_PRIVATE_KEY) {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+    
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey,
+      }),
+    });
+  } else {
+    // For local
+    const serviceAccountPath = path.resolve(process.cwd(), '../SECRETS/conference-companion-d4f5a-firebase-adminsdk-fbsvc-c5f5e7dbb1.json');
+    
+    if (!fs.existsSync(serviceAccountPath)) {
+      console.warn("Firebase admin was not initialized, the key is not found.");
+    } else {
+      const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    }
   }
-
-  const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
 }
 
 const adminAuth = admin.auth();
